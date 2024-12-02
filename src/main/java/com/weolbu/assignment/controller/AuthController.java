6afa61dbba226952095b350aaf9089fd2db5bd06
eqvyoo <ys07106@gmail.com>
2024-12-02 20:49:20 +0900

    feat : 로그인 API swagger 문서 추가

--- src/main/java/com/weolbu/assignment/controller/AuthController.java
+++ src/main/java/com/weolbu/assignment/controller/AuthController.java
@@ -3,17 +3,19 @@ package com.weolbu.assignment.controller;
 import com.weolbu.assignment.dto.LoginRequest;
 import com.weolbu.assignment.dto.LoginResponse;
 import com.weolbu.assignment.service.AuthService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-
+@Tag(name = "인증 API", description = "인증 관련 API를 제공합니다.")
 @RestController
 @RequestMapping("/api/auth")
 @RequiredArgsConstructor
 public class AuthController {
     private final AuthService authService;
-
+    @Operation(summary = "로그인", description = "로그인과 함께 토큰을 발급합니다.")
     @PostMapping("/login")
     public ResponseEntity<LoginResponse> login(@Valid @RequestBody LoginRequest request) {
         LoginResponse response = authService.login(request);
