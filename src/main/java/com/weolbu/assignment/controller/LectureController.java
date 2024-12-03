38fd011880d33ea7cc4004008f7153fde92760bf
eqvyoo <ys07106@gmail.com>
2024-12-03 13:27:48 +0900

    docs : 강의 개설 기능 swagger 어노테이션 추가

--- src/main/java/com/weolbu/assignment/controller/LectureController.java
+++ src/main/java/com/weolbu/assignment/controller/LectureController.java
@@ -2,6 +2,8 @@ package com.weolbu.assignment.controller;
 
 import com.weolbu.assignment.dto.LectureCreateRequest;
 import com.weolbu.assignment.service.LectureService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
@@ -13,6 +15,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@Tag(name = "강의 API", description = "강 관련 API를 제공합니다.")
 @RequestMapping("/api/lectures")
 @RequiredArgsConstructor
 public class LectureController {
@@ -20,6 +23,7 @@ public class LectureController {
     private final LectureService lectureService;
 
     @PostMapping
+    @Operation(summary = "강의 개설 기능", description = "강사 회원은 강의를 개설할 수 있습니다.")
     public ResponseEntity<String> createLecture(
             @AuthenticationPrincipal UserDetails userDetails,
             @Valid @RequestBody LectureCreateRequest lectureRequest
